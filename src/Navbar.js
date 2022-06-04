95be5c0f222b18c0edd32d868b1b40325b2c4eff
Aaron Reyes <ProdAronBeatz@gmail.com>
2022-06-04 00:34:56 -0500

    Started a new project working on the fundamentals on making an official site

---
+++ src/Navbar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import logo from "./images/logo.svg";
+
+const Navbar = () => {
+  return <h2>Navbar Component</h2>;
+};
+
+export default Navbar;
---
+++ src/Sidebar.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { FaTimes } from "react-icons/fa";
+import subLinks from "./Data";
+
+const Sidebar = () => {
+  return <h2>Sidebar Component</h2>;
+};
+
+export default Sidebar;
